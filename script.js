d91d53e417ae4dcf8647b34dd8c2c8ff7ec138a1
sanjanamentes <144243066+sanjanamentes@users.noreply.github.com>
2023-09-19 19:42:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function submits(){
+   var Namec1=parseInt(document.getElementById("namef").value);
+    var Namec2=parseInt(document.getElementById("namel").value); 
+  var Datec1=parseInt(document.getElementById("datec").value);
+   var Emailc1=parseInt(document.getElementById("emailc").value);
+   var Phone=parseInt(document.getElementById("phonec").value);
+    alert("FORM SUBMITTED SUCESSFULLY");
+
+}
